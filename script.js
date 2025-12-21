ee99582170baa129d4d3d30f99b6e2b1a4f699ec
Anurag Shivarathri <anurag6713@gmail.com>
2025-12-21 06:36:49 +0530

    Update script.js Test 3

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // script.js
 
-// console.log('testing..2');
+// console.log('testing..3');
 
 document.addEventListener("DOMContentLoaded", () => {
   /* ===== FAQ ACCORDION (one open at a time) ===== */
